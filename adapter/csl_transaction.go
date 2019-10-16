5d1715ac0871337bcc0337235b8cf0f7979d4bf1
zhangyasong <zhang.yasong@eland.co.kr>
2019-10-16 14:10:19 +0800

    #76 Modify InDateTime and ModiDateTime

--- adapter/csl_transaction.go
+++ adapter/csl_transaction.go
@@ -119,7 +119,6 @@ func (etl ClearanceToCslETL) Transform(ctx context.Context, source interface{}) 
 	saleMsts := make([]models.SaleMst, 0)
 	saleDtls := make([]models.SaleDtl, 0)
 	salePayments := make([]models.SalePayment, 0)
-	local, _ := time.ParseDuration("8h")
 	for i, saleTransaction := range saleTAndSaleTDtls.SaleTransactions {
 		saleDate := saleTransaction.SaleDate.Format("20060102")
 
@@ -284,9 +283,7 @@ func (etl ClearanceToCslETL) Transform(ctx context.Context, source interface{}) 
 			ActualSaleAmt:               saleTransaction.TotalTransactionPrice - feeAmt,
 			ObtainMileage:               mileage.Point,
 			InUserID:                    colleagues.UserName,
-			InDateTime:                  (saleTransaction.SaleDate).Add(local),
 			ModiUserID:                  colleagues.UserName,
-			ModiDateTime:                (saleTransaction.SaleDate).Add(local),
 			SendState:                   "",
 			SendFlag:                    NotSynChronized,
 			DiscountAmtAsCost:           0,
@@ -628,9 +625,7 @@ func (etl ClearanceToCslETL) Transform(ctx context.Context, source interface{}) 
 					NormalFeeRate:                     normalFeeRate,
 					SaleEventFeeRate:                  saleEventFeeRate,
 					InUserID:                          colleagues.UserName,
-					InDateTime:                        (saleTransaction.SaleDate).Add(local),
 					ModiUserID:                        colleagues.UserName,
-					ModiDateTime:                      (saleTransaction.SaleDate).Add(local),
 					SendState:                         "",
 					SendFlag:                          NotSynChronized,
 					DiscountAmt:                       discountAmt,
@@ -696,9 +691,7 @@ func (etl ClearanceToCslETL) Transform(ctx context.Context, source interface{}) 
 				PaymentCode:        pop.PaymentCode,
 				PaymentAmt:         pop.PaymentAmt,
 				InUserID:           colleagues.UserName,
-				InDateTime:         (pop.InDateTime).Add(local),
 				ModiUserID:         colleagues.UserName,
-				ModiDateTime:       (pop.ModiDateTime).Add(local),
 				SendFlag:           "R",
 				CreditCardFirmCode: creditCardFirmCode,
 				TransactionId:      saleMst.TransactionId,
@@ -756,7 +749,7 @@ func (etl ClearanceToCslETL) Load(ctx context.Context, source interface{}) error
 	//get engine
 	engine := factory.GetCSLEngine()
 	engine.SetMapper(core.SameMapper{})
-
+	engine.TZLocation, _ = time.LoadLocation("Asia/Shanghai")
 	//create session
 	session := engine.NewSession()
 	defer session.Close()
