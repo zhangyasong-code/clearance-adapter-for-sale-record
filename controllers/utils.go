3b40892e3c6d91bed6a43941a837b05f87dde423
liuwenbin <liu.wenbin@eland.co.kr>
2019-12-26 13:56:49 +0800

    #237 change getSaleTransactions api

--- controllers/utils.go
+++ controllers/utils.go
@@ -1,7 +1,10 @@
 package controllers
 
 import (
+	"errors"
+	"fmt"
 	"strings"
+	"time"
 )
 
 func splitTolist(s string) []string {
@@ -12,4 +15,27 @@ func splitTolist(s string) []string {
 		}
 	}
 	return list
+}
+
+func DateTimeValidate(startTime time.Time, endTime time.Time) error {
+	term := 30
+	fmt.Println(startTime)
+	fmt.Println(endTime)
+
+	if startTime.IsZero() && endTime.IsZero() {
+		return nil
+	}
+	if startTime.IsZero() && !endTime.IsZero() {
+		return errors.New("开始时间不能为空")
+	}
+	if !startTime.IsZero() && endTime.IsZero() {
+		return errors.New("结束时间不能为空")
+	}
+	if startTime.After(endTime) {
+		return errors.New("开始时间不能大于结束时间")
+	}
+	if startTime.AddDate(0, 0, term-1).Before(endTime) {
+		return errors.New(fmt.Sprintf("查询期间不能大于%d天", term))
+	}
+	return nil
 }
